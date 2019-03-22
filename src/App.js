75cfcb53cb6ff8dbc517e5f3a794ce7c5285a81a
SPavelV <pashius2010@gmail.com>
2019-03-22 10:34:59 +0400

    add logo, react-svg plugin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import MainNav from './components/main-nav/MainNav'
+import RouterLink from './components/router/Link';
+import ReactSVG from 'react-svg';
 
 class App extends Component {
   constructor(props) {
@@ -33,15 +35,28 @@ class App extends Component {
     }
     return (
       <div className="app">
-        <MainNav/>
-        {this.state.loading ? (
-          <div className="loading">
-            {/* <Loader/> */}
-          </div>
-        ) : (
-          
-          this.props.children 
-        )}
+        <header className="app__header">
+          <RouterLink to="/">
+            <a href="/" className="app__logo">
+              <ReactSVG 
+              evalScripts="always"
+              src = "./assets/images/icons/logo.svg" 
+              svgClassName="app__logo-icon"
+              svgStyle={{ width: 100 }}
+              wrapper="span"
+              />
+            </a>
+          </RouterLink>
+          <MainNav/>
+          {this.state.loading ? (
+            <div className="loading">
+              {/* <Loader/> */}
+            </div>
+          ) : (
+            
+            this.props.children 
+          )}
+        </header>
       </div>
     );
   }
